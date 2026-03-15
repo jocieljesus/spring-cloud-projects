35e7677898b76899798126632e5c548b5dbbc9f0
jociel <jocielalvesdejesus@gmail.com>
2026-03-15 16:00:18 -0300

    feat: projeto de gateway

---
+++ gateway-service/src/main/java/com/alvez/gateway_service/Application.java
@@ -0,0 +1,14 @@
+package com.alvez.gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
